0fe50968809cf1a1864be45735d6c19c4e4c0a0d
HailinPan <hailinpan1988@163.com>
2026-08-06 10:47:32 +0800

    update

---
+++ src/dicella/__init__.py
@@ -0,0 +1,8 @@
+"""DiCellA: Digital Cell Analysis Toolkit."""
+
+try:
+    from ._version import version as __version__
+except ImportError:
+    __version__ = "0.0.0.dev0"
+
+__all__ = ["__version__"]
